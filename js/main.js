7a23185e42dc742cb7eca5b35e84dbffee68ba7e
AbdelhameedMarei <abdelhameed.mahmoud000@mail.ru>
2022-06-29 19:15:33 +0300

    add nice scroll and update some section dynamic by js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,23 @@ btn.onclick = function () {
     behavior: "smooth",
   });
 };
+
+//////////////////////////////////////////////////
+
+//Nice Scroll Plugin
+$(document).ready(function () {
+  $("html").niceScroll({
+    cursorwidth: "12px",
+    cursorcolor: "#33d1cc",
+    autohidemode: true,
+    zindex: 9999,
+    mousescrollstep: 100,
+  });
+});
+
+////////////////////////////////////////////////////
+
+//Current Year in Section Footer
+let theYear = document.querySelector(".theyear");
+let dateNow = new Date();
+theYear.textContent = dateNow.getFullYear(); //Get Year Now
